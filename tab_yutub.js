07209e4c13cbc10ecba247cd67c9756ddfe5e1fd
fiqiez <fiqiez@gmail.com>
2016-03-11 19:50:39 +0700

    upd data

--- tab_yutub.js
+++ tab_yutub.js
@@ -1,4 +1,4 @@
 var expData = [
-{"yutub_account_id":1,"yutub_ads_title":"Langkah Yang Harus Dilakukan Sebelum Menjual Handphone","yutub_ads_status":1,"yutub_ads_date":"2016-02-24T08:27:49.888Z","yutub_last_up":"2016-03-08T11:09:40.450Z","yutub_last_down":"2016-03-08T11:03:48.166Z","yutub_ads_id":1},
-{"yutub_account_id":2,"yutub_ads_title":"Langkah Yang Harus Dilakukan Sebelum Menjual Handphone","yutub_ads_status":1,"yutub_ads_date":"2016-02-24T08:29:29.601Z","yutub_last_up":"2016-03-08T11:25:28.401Z","yutub_last_down":"2016-03-08T11:18:49.540Z","yutub_ads_id":2}
+{"yutub_account_id":1,"yutub_ads_title":"Langkah Yang Harus Dilakukan Sebelum Menjual Handphone","yutub_ads_status":1,"yutub_ads_date":"2016-02-24T08:27:49.888Z","yutub_last_up":"2016-03-11T12:41:30.677Z","yutub_last_down":"2016-03-11T12:40:54.696Z","yutub_ads_id":1},
+{"yutub_account_id":2,"yutub_ads_title":"Langkah Yang Harus Dilakukan Sebelum Menjual Handphone","yutub_ads_status":1,"yutub_ads_date":"2016-02-24T08:29:29.601Z","yutub_last_up":"2016-03-11T12:34:34.289Z","yutub_last_down":"2016-03-11T12:33:33.016Z","yutub_ads_id":2}
 ]
